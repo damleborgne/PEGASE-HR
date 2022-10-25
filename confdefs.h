4e301ce2aa98853e774aaf863051b270e912f36e
Damien Le Borgne <damleborgne@yahoo.fr>
2022-10-25 18:41:32 +0200

    cleanup config files

--- confdefs.h
+++
@@ -1,8 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "PEGASE.HR"
-#define PACKAGE_TARNAME "pegase-hr"
-#define PACKAGE_VERSION "-"
-#define PACKAGE_STRING "PEGASE.HR -"
-#define PACKAGE_BUGREPORT "leborgne@iap.fr"
-#define PACKAGE_URL ""
-#define PL_VERSION "2.0.2"
