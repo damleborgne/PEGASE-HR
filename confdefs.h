8bad960cd7e9d558e3001afd2d57630a61f442f5
Damien Le Borgne <damleborgne@yahoo.fr>
2022-10-25 17:40:06 +0200

    remove cfitsio

---
+++ confdefs.h
@@ -0,0 +1,8 @@
+/* confdefs.h */
+#define PACKAGE_NAME "PEGASE.HR"
+#define PACKAGE_TARNAME "pegase-hr"
+#define PACKAGE_VERSION "-"
+#define PACKAGE_STRING "PEGASE.HR -"
+#define PACKAGE_BUGREPORT "leborgne@iap.fr"
+#define PACKAGE_URL ""
+#define PL_VERSION "2.0.2"
